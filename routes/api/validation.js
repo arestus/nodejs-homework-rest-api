a246c2730926ce123b6ddf9b4ea84705f409b35c
arestus <arestus@gmail.com>
2021-05-22 17:03:29 +0300

    add comments for validation fails

--- routes/api/validation.js
+++ routes/api/validation.js
@@ -2,7 +2,7 @@ const Joi = require("joi");
 
 const schemaAddContact = Joi.object({
   name: Joi.string().min(3).max(30).required().messages({
-    "any.required": `LARISA NE DOBIVAY MENYA PLS`,
+    "any.required": "missing required name field",
   }),
   email: Joi.string()
     .email({
@@ -10,30 +10,25 @@ const schemaAddContact = Joi.object({
       tlds: { allow: ["com", "net"] },
     })
     .messages({
-      "any.required": `"email" is a required field`,
+      "any.required": "missing required email field",
     })
     .required(),
   phone: Joi.string().min(6).max(20).required().messages({
-    "any.required": `"phone" is a required field`,
+    "any.required": "missing required phone field",
   }),
 });
 
 const schemaUpdateContact = Joi.object({
-  name: Joi.string().min(3).max(30).required().messages({
-    "any.required": `"name" is a required field`,
-  }),
+  name: Joi.string().min(3).max(30).required(),
   email: Joi.string()
     .email({
       minDomainSegments: 2,
       tlds: { allow: ["com", "net"] },
     })
-    .messages({
-      "any.required": `"email" is a required field`,
-    })
     .required(),
-  phone: Joi.string().min(6).max(20).required().messages({
-    "any.required": `"phone" is a required field`,
-  }),
+  phone: Joi.string().min(6).max(20).required(),
+}).messages({
+  "any.required": "missing fields",
 });
 
 const validate = async (schema, obj, next) => {
